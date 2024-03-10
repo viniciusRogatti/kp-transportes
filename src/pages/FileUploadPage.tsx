fbaa06c24936b187bcf7274700c6be21e342e661
Marcos Vinicius <vinnyzsouza@gmail.com>
2024-03-10 17:20:28 -0300

    configurando timeout para ler arquivos .xml

--- src/pages/FileUploadPage.tsx
+++ src/pages/FileUploadPage.tsx
@@ -6,6 +6,7 @@ import { Container } from '../style/incoives';
 
 const FileUploadPage: React.FC = () => {
   const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+  const [uploading, setUploading] = useState(false);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
@@ -16,26 +17,33 @@ const FileUploadPage: React.FC = () => {
   };
 
   const handleUpload = async () => {
-    if (selectedFiles.length > 0) {
+    if (selectedFiles.length > 0 && !uploading) {
+      setUploading(true);
+
       const formData = new FormData();
       selectedFiles.forEach(file => {
-        console.log(file);
-        
         formData.append('files', file);
       });
 
       try {
         await axios.post(`${URL}/upload`, formData, {
           headers: {
-            'Content-Type': 'multipart/form-data'
-          }
+            'Content-Type': 'multipart/form-data',
+          },
         });
         console.log('Arquivos enviados com sucesso!');
       } catch (error) {
         console.error('Erro ao enviar arquivos:', error);
+      } finally {
+        setUploading(false);
+
+        // Agora, configuramos um timeout de 1 segundo antes de permitir a próxima solicitação
+        setTimeout(() => {
+          // O timeout expirou, permitindo a próxima solicitação
+        }, 100);
       }
     } else {
-      console.error('Nenhum arquivo .xml selecionado.');
+      console.error('Nenhum arquivo .xml selecionado ou upload em andamento.');
     }
   };
 
@@ -44,7 +52,9 @@ const FileUploadPage: React.FC = () => {
       <Header />
       <Container>
         <input type="file" onChange={handleFileChange} multiple accept=".xml" />
-        <button onClick={handleUpload}>Enviar Arquivos .xml</button>
+        <button onClick={handleUpload} disabled={uploading}>
+          Enviar Arquivos .xml
+        </button>
       </Container>
     </div>
   );
